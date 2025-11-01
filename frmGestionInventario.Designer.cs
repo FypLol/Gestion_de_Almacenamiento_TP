54322f39b9d153062f8ca344619da5b735abf709
Joseth <Joseth@DESKTOP-HBB5D0V>
2025-11-01 15:59:44 -0500

    Agregar archivos de proyecto.

---
+++ frmGestionInventario.Designer.cs
@@ -0,0 +1,294 @@
+﻿namespace Gestion_de_Almacenamiento_TP
+{
+    partial class frmGestionInventario
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button3 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lbl_nombre = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.tbox_celular = new System.Windows.Forms.TextBox();
+            this.lbl_peso = new System.Windows.Forms.Label();
+            this.lbl_celular = new System.Windows.Forms.Label();
+            this.lbl_tipodemerca = new System.Windows.Forms.Label();
+            this.tbox_dni = new System.Windows.Forms.TextBox();
+            this.lbl_origen = new System.Windows.Forms.Label();
+            this.lbl_dni = new System.Windows.Forms.Label();
+            this.tbox_nombre = new System.Windows.Forms.TextBox();
+            this.tbox_tipodemercaderia = new System.Windows.Forms.TextBox();
+            this.tbox_origen = new System.Windows.Forms.TextBox();
+            this.tbox_peso = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.Crimson;
+            this.button3.FlatAppearance.BorderSize = 0;
+            this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.ForeColor = System.Drawing.SystemColors.Control;
+            this.button3.Location = new System.Drawing.Point(687, 225);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(278, 37);
+            this.button3.TabIndex = 32;
+            this.button3.Text = "ELIMINAR";
+            this.button3.UseVisualStyleBackColor = false;
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.Tan;
+            this.button2.FlatAppearance.BorderSize = 0;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.ForeColor = System.Drawing.SystemColors.Control;
+            this.button2.Location = new System.Drawing.Point(403, 225);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(278, 37);
+            this.button2.TabIndex = 31;
+            this.button2.Text = "EDITAR";
+            this.button2.UseVisualStyleBackColor = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(371, 37);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(360, 55);
+            this.label1.TabIndex = 30;
+            this.label1.Text = "Datos del Envio";
+            // 
+            // lbl_nombre
+            // 
+            this.lbl_nombre.AutoSize = true;
+            this.lbl_nombre.Location = new System.Drawing.Point(530, 113);
+            this.lbl_nombre.Name = "lbl_nombre";
+            this.lbl_nombre.Size = new System.Drawing.Size(100, 13);
+            this.lbl_nombre.TabIndex = 29;
+            this.lbl_nombre.Text = "Nombre/Raz.Social";
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Chartreuse;
+            this.button1.FlatAppearance.BorderSize = 0;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.SystemColors.Control;
+            this.button1.Location = new System.Drawing.Point(113, 225);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(278, 37);
+            this.button1.TabIndex = 17;
+            this.button1.Text = "AGREGAR";
+            this.button1.UseVisualStyleBackColor = false;
+            // 
+            // tbox_celular
+            // 
+            this.tbox_celular.Location = new System.Drawing.Point(687, 194);
+            this.tbox_celular.Name = "tbox_celular";
+            this.tbox_celular.Size = new System.Drawing.Size(266, 20);
+            this.tbox_celular.TabIndex = 28;
+            // 
+            // lbl_peso
+            // 
+            this.lbl_peso.AutoSize = true;
+            this.lbl_peso.Location = new System.Drawing.Point(-118, 167);
+            this.lbl_peso.Name = "lbl_peso";
+            this.lbl_peso.Size = new System.Drawing.Size(31, 13);
+            this.lbl_peso.TabIndex = 18;
+            this.lbl_peso.Text = "Peso";
+            // 
+            // lbl_celular
+            // 
+            this.lbl_celular.AutoSize = true;
+            this.lbl_celular.Location = new System.Drawing.Point(565, 197);
+            this.lbl_celular.Name = "lbl_celular";
+            this.lbl_celular.Size = new System.Drawing.Size(56, 13);
+            this.lbl_celular.TabIndex = 27;
+            this.lbl_celular.Text = "CELULAR";
+            // 
+            // lbl_tipodemerca
+            // 
+            this.lbl_tipodemerca.AutoSize = true;
+            this.lbl_tipodemerca.Location = new System.Drawing.Point(-150, 206);
+            this.lbl_tipodemerca.Name = "lbl_tipodemerca";
+            this.lbl_tipodemerca.Size = new System.Drawing.Size(100, 13);
+            this.lbl_tipodemerca.TabIndex = 19;
+            this.lbl_tipodemerca.Text = "Tipo de mercadería";
+            // 
+            // tbox_dni
+            // 
+            this.tbox_dni.Location = new System.Drawing.Point(687, 145);
+            this.tbox_dni.Name = "tbox_dni";
+            this.tbox_dni.Size = new System.Drawing.Size(266, 20);
+            this.tbox_dni.TabIndex = 26;
+            // 
+            // lbl_origen
+            // 
+            this.lbl_origen.AutoSize = true;
+            this.lbl_origen.Location = new System.Drawing.Point(-128, 248);
+            this.lbl_origen.Name = "lbl_origen";
+            this.lbl_origen.Size = new System.Drawing.Size(41, 13);
+            this.lbl_origen.TabIndex = 20;
+            this.lbl_origen.Text = "Origen:";
+            // 
+            // lbl_dni
+            // 
+            this.lbl_dni.AutoSize = true;
+            this.lbl_dni.Location = new System.Drawing.Point(565, 152);
+            this.lbl_dni.Name = "lbl_dni";
+            this.lbl_dni.Size = new System.Drawing.Size(26, 13);
+            this.lbl_dni.TabIndex = 25;
+            this.lbl_dni.Text = "DNI";
+            // 
+            // tbox_nombre
+            // 
+            this.tbox_nombre.Location = new System.Drawing.Point(687, 106);
+            this.tbox_nombre.Name = "tbox_nombre";
+            this.tbox_nombre.Size = new System.Drawing.Size(266, 20);
+            this.tbox_nombre.TabIndex = 21;
+            // 
+            // tbox_tipodemercaderia
+            // 
+            this.tbox_tipodemercaderia.Location = new System.Drawing.Point(208, 145);
+            this.tbox_tipodemercaderia.Name = "tbox_tipodemercaderia";
+            this.tbox_tipodemercaderia.Size = new System.Drawing.Size(247, 20);
+            this.tbox_tipodemercaderia.TabIndex = 24;
+            // 
+            // tbox_origen
+            // 
+            this.tbox_origen.Location = new System.Drawing.Point(208, 190);
+            this.tbox_origen.Name = "tbox_origen";
+            this.tbox_origen.Size = new System.Drawing.Size(247, 20);
+            this.tbox_origen.TabIndex = 22;
+            // 
+            // tbox_peso
+            // 
+            this.tbox_peso.Location = new System.Drawing.Point(208, 106);
+            this.tbox_peso.Name = "tbox_peso";
+            this.tbox_peso.Size = new System.Drawing.Size(247, 20);
+            this.tbox_peso.TabIndex = 23;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(120, 106);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(31, 13);
+            this.label2.TabIndex = 33;
+            this.label2.Text = "Peso";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(88, 145);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(100, 13);
+            this.label3.TabIndex = 34;
+            this.label3.Text = "Tipo de mercadería";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(110, 187);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(41, 13);
+            this.label4.TabIndex = 35;
+            this.label4.Text = "Origen:";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.BackgroundColor = System.Drawing.Color.AliceBlue;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(20, 268);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(1014, 389);
+            this.dataGridView1.TabIndex = 36;
+            // 
+            // frmGestionInventario
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1057, 669);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lbl_nombre);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.tbox_celular);
+            this.Controls.Add(this.lbl_peso);
+            this.Controls.Add(this.lbl_celular);
+            this.Controls.Add(this.lbl_tipodemerca);
+            this.Controls.Add(this.tbox_dni);
+            this.Controls.Add(this.lbl_origen);
+            this.Controls.Add(this.lbl_dni);
+            this.Controls.Add(this.tbox_nombre);
+            this.Controls.Add(this.tbox_tipodemercaderia);
+            this.Controls.Add(this.tbox_origen);
+            this.Controls.Add(this.tbox_peso);
+            this.Name = "frmGestionInventario";
+            this.Text = "frmGestionInventario";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lbl_nombre;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox tbox_celular;
+        private System.Windows.Forms.Label lbl_peso;
+        private System.Windows.Forms.Label lbl_celular;
+        private System.Windows.Forms.Label lbl_tipodemerca;
+        private System.Windows.Forms.TextBox tbox_dni;
+        private System.Windows.Forms.Label lbl_origen;
+        private System.Windows.Forms.Label lbl_dni;
+        private System.Windows.Forms.TextBox tbox_nombre;
+        private System.Windows.Forms.TextBox tbox_tipodemercaderia;
+        private System.Windows.Forms.TextBox tbox_origen;
+        private System.Windows.Forms.TextBox tbox_peso;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.DataGridView dataGridView1;
+    }
+}
